c5fe866a5e801d042ace1d31e7a454dc31ea1b4c
ARBIEV <52335415+Temka31@users.noreply.github.com>
2020-02-07 17:37:08 +0300

    Update Drawer.js

--- components/Drawer.js
+++ components/Drawer.js
@@ -3,6 +3,7 @@ import { makeStyles } from '@material-ui/core/styles';
 import Drawer from '@material-ui/core/Drawer';
 import Button from '@material-ui/core/Button';
 import List from '@material-ui/core/List';
+import MenuIcon from '@material-ui/icons/Menu';
 
 
 const useStyles = makeStyles({
@@ -43,7 +44,7 @@ export default function TemporaryDrawer() {
 
   return (
     <div>
-      <Button onClick={toggleDrawer('left', true)}>Open Left</Button>
+      <Button onClick={toggleDrawer('left', true)}><MenuIcon /></Button>
       <Drawer open={state.left} onClose={toggleDrawer('left', false)}>
         {sideList('left')}
       </Drawer>
