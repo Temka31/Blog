a8ed3e47416392302d449f890cb70986cc8e4376
ARBIEV <52335415+Temka31@users.noreply.github.com>
2020-02-07 17:57:20 +0300

    Update header.js

--- components/header.js
+++ components/header.js
@@ -1,39 +1,23 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import TemporaryDrawer from "./Drawer";
+import React from "react";
+import TemporaryDrawer from "./Drawer"
 
-const useStyles = makeStyles(theme => ({
+const styles = {
   root: {
-    flexGrow: 1,
-  },
-  menuButton: {
-    marginRight: theme.spacing(2),
-  },
-  title: {
-    flexGrow: 1,
-  },
-}));
+    display: "flex",
+    position: "static",
+    flexShrink: 0,
+    height: "50px",
+    background: "blue",
+    flexDirection: "column",
+    flexWrap: "nowrap"
+  }
+};
 
 export default function Header() {
-  const classes = useStyles();
-
   return (
-    <div className={classes.root}>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-            <TemporaryDrawer/>
-          </IconButton>
-          <Typography variant="h6" className={classes.title}>
-            News
-          </Typography>
-        </Toolbar>
-      </AppBar>
-    </div>
+    <header style={styles.root}>
+    <TemporaryDrawer/> 
+    <h1>News</h1>
+    </header>
   );
 }
